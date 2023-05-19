0936dc993a3b9f7443b4525a65fe215197c6a19d
Oyedare <ooyedaretemiloluwa@gmail.com>
2023-05-19 16:22:10 +0100

    navbar component completed

---
+++ src/components/cta.jsx
@@ -0,0 +1,17 @@
+import '../App.css';
+
+function Cta() {
+  return (
+    <div className='cta'>
+        <div className='cta-text'>
+            <h1>Make your party fun!</h1>
+            <p>Create your own custom playlist today</p>
+        </div>
+        <div className="cta-btn">
+            Create playlist
+        </div>
+    </div>
+  );
+}
+
+export default Cta;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import '../App.css';
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <a href='#'>Contact Us</a>
+        <div>
+            <a href='#'>Signup</a>
+        </div>
+    </div>
+  );
+}
+
+export default Navbar;
